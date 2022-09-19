ea3aeab23a42a0f6ede2629969930aa0e8d1b3cd
jngs-park <dear.jayp@gmail.com>
2022-09-19 15:44:18 +0900

    mbti, switch

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -1,13 +1,11 @@
 package com.ll.exam.sbb;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
 @Controller
 public class MainController {
     private int increaseNo = -1;
@@ -52,16 +50,13 @@ public class MainController {
     public int showPlus(int a, int b) {
         return a + b;
     }
-
     @GetMapping("/plus2")
     @ResponseBody
     public void showPlus2(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         int a = Integer.parseInt(req.getParameter("a"));
         int b = Integer.parseInt(req.getParameter("b"));
-
         resp.getWriter().append(a + b + "");
     }
-
     @GetMapping("/minus")
     @ResponseBody
     public int showMinus(int a, int b) {
@@ -80,5 +75,17 @@ public class MainController {
         return IntStream.rangeClosed(1, limit)
                 .mapToObj(i -> "%d * %d = %d".formatted(finalDan, i, finalDan * i))
                 .collect(Collectors.joining("<br>\n"));
+    }
+
+    @GetMapping("/mbti/{name}")
+    @ResponseBody
+    public String showMbti(@PathVariable String name) {
+        return switch (name) {
+            case "홍길동" -> "INFP";
+            case "홍길순" -> "INFJ";
+            case "임꺽정" -> "ENFP";
+            case "장희성" -> "INFP";
+            default -> "모름";
+        };
     }
 }
