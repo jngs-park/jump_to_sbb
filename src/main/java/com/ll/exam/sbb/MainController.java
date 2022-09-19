6356000e5ded0d5286d06e02aa55cf9025594a6d
jngs-park <dear.jayp@gmail.com>
2022-09-19 14:26:36 +0900

    요청 관련 문제

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -1,9 +1,11 @@
 package com.ll.exam.sbb;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
 @Controller
 public class MainController {
+    private int increaseNo = -1;
+
     @RequestMapping("/sbb")
     // 아래 함수의 리턴값을 그대로 브라우저에 표시
     // 아래 함수의 리턴값을 문자열화 해서 브라우저 응답의 바디에 담는다.
@@ -12,10 +14,8 @@ public class MainController {
         // 서버에서 출력
         System.out.println("Hello");
         // 먼 미래에 브라우저에서 보여짐
-        return "안녕하세요.222222222";
+        return "안녕하세요.";
     }
-
-
     @GetMapping("/page1")
     @ResponseBody
     public String showPage1() {
@@ -26,7 +26,6 @@ public class MainController {
                 </form>
                 """;
     }
-
     @PostMapping("/page2")
     @ResponseBody
     public String showPage2Post(@RequestParam(defaultValue = "0") int age) {
@@ -35,7 +34,6 @@ public class MainController {
                 <h1>안녕하세요, POST 방식으로 오셨군요.</h1>
                 """.formatted(age);
     }
-
     @GetMapping("/page2")
     @ResponseBody
     public String showPage2Get(@RequestParam(defaultValue = "0") int age) {
@@ -43,5 +41,25 @@ public class MainController {
                 <h1>입력된 나이 : %d</h1>
                 <h1>안녕하세요, POST 방식으로 오셨군요.</h1>
                 """.formatted(age);
+    }
+
+    @GetMapping("/plus")
+    @ResponseBody
+    public int showPlus(int a, int b) {
+        return a + b;
+    }
+
+    @GetMapping("/minus")
+    @ResponseBody
+    public int showMinus(int a, int b) {
+        return a - b;
+    }
+
+    @GetMapping("/increase")
+    @ResponseBody
+    public int showIncrease() {
+        increaseNo++;
+
+        return increaseNo;
     }
 }
