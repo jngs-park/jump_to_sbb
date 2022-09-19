776d0d714aacc54a9b20313275ca7f263c639602
jngs-park <dear.jayp@gmail.com>
2022-09-19 20:31:30 +0900

    Optional 관련 오류 수정

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -136,7 +136,8 @@ public class MainController {
                 .stream()
                 .filter(a -> a.getId() == id) // 1번
                 .findFirst()
-                .get();
+                .orElse(null);
+
 
         return article;
     }
@@ -148,10 +149,11 @@ public class MainController {
                 .stream()
                 .filter(a -> a.getId() == id) // 1번
                 .findFirst()
-                .get();
+                .orElse(null);
+
 
         if (article == null) {
-            return "%d번 게시물은 존재하지 않습니다.".formatted(article.getId());
+            return "%d번 게시물은 존재하지 않습니다.".formatted(id);
         }
 
         article.setTitle(title);
