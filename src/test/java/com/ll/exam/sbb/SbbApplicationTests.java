1d82aae6a56245602cc86efa99d5959f97de737a
jngs-park <dear.jayp@gmail.com>
2022-09-26 01:24:30 +0900

    question truncate 테스트케이스 추가

--- src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
@@ -19,7 +19,24 @@ class SbbApplicationTests {
 	void contextLoads() {
 	}
 	@Test
-	void testJpa() {
+	void testJpa0() {
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q1);
+
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q2);
+
+		questionRepository.truncate();
+	}
+
+	@Test
+	void testJpa1() {
 		Question q1 = new Question();
 		q1.setSubject("sbb가 무엇인가요?");
 		q1.setContent("sbb에 대해서 알고 싶습니다.");
@@ -49,20 +66,20 @@ class SbbApplicationTests {
 	}
 	@Test
 	void testJpa4() {
-		Question q = this.questionRepository.findBySubjectAndContent(
+		Question q = questionRepository.findBySubjectAndContent(
 				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
 		assertEquals(1, q.getId());
 	}
 	@Test
 	void testJpa5() {
-		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+		List<Question> qList = questionRepository.findBySubjectLike("sbb%");
 		Question q = qList.get(0);
 		assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
 
 	@Test
 	void testJpa6() {
-		Optional<Question> oq = this.questionRepository.findById(1);
+		Optional<Question> oq = questionRepository.findById(1);
 		assertTrue(oq.isPresent());
 		Question q = oq.get();
 		q.setSubject("수정된 제목");
@@ -71,13 +88,13 @@ class SbbApplicationTests {
 
 	@Test
 	void testJpa7() {
-		assertEquals(2, this.questionRepository.count());
+		assertEquals(2, questionRepository.count());
 		Optional<Question> oq = this.questionRepository.findById(1);
 		assertTrue(oq.isPresent());
 		Question q = oq.get();
 
-		this.questionRepository.delete(q);
+		questionRepository.delete(q);
 
-		assertEquals(1, this.questionRepository.count());
+		assertEquals(1, questionRepository.count());
 	}
 }
